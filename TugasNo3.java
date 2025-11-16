89eb20d7b461f7c34fe10f800c2272461170c2d6
Aureylio <aureyliorangga2@gmail.com>
2025-11-16 09:58:48 +0700

    TugasNo 3

--- TugasNo3.java
+++ TugasNo3.java
@@ -1,33 +1,31 @@
 import java.util.Scanner;
-
 public class TugasNo3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner (System.in);
 
-        // Menu tersedia
-        String[] menu = {
-            "Nasi Goreng", "Mie Goreng", "Roti Bakar",
-            "Kentang Goreng", "Teh Tarik", "Cappucino", "Chocolate Ice"
-        };
-
-        // Input makanan yang ingin dicari
-        System.out.print("Masukkan nama makanan yang ingin dicari: ");
-        String cari = sc.nextLine();
-
-        // Linear Search
+        // daftar menu yang tersedia
+        String[] menu ={"Nasi Goreng", "Mie Goreng", 
+        "Roti Bakar", "Kentang Goreng", "Teh Tarik", "Cappucino", "Chocolate Ice"};
+        
+        // nama makanan yang ingin dicari (input dari pengguna)
+        System.out.print("Masukkan nama makananan yang ingin dicari: ");
+        String cari = scanner.nextLine();
+        
+        // Algoritma Linear Search
         boolean ditemukan = false;
+
         for (int i = 0; i < menu.length; i++) {
             if (menu[i].equalsIgnoreCase(cari)) {
-                ditemukan = true;
-                break;
+               ditemukan = true;
+               break;
             }
         }
 
-        // Output hasil
+        // output hasil 
         if (ditemukan) {
-            System.out.println("Makanan \"" + cari + "\" tersedia di menu.");
+        System.out.println("Makanan \"" + cari + "\" tersedia di menu.");
         } else {
-            System.out.println("Maaf, makanan \"" + cari + "\" tidak ada di menu.");
-        }
+            System.out.println("Makanan \"" + cari + "\" tidak tersedia di menu.");
+    }
     }
 }
